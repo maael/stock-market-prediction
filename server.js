27699647590c745cb2492e1f8ef8f6615847c794
maael <mae.me.uk@gmail.com>
2015-03-22 16:04:15 +0000

    Move app into server.js, have index.js call server.js

---
+++ server.js
@@ -0,0 +1,49 @@
+var express = require('express'),
+    pub = __dirname + '/public',
+    app = express(),
+    port = process.env.PORT || 3000,
+    routes = require('./app/routes'),
+    cookieParser = require('cookie-parser'),
+    bodyParser = require('body-parser'),
+    session = require('express-session'),
+    flash = require('connect-flash'),
+    mongoose = require('mongoose'),
+    morgan = require('morgan'),
+    passport = require('passport'),
+    pm2 = require('pm2');
+
+function getApp(dbURL) {
+  /* Configure db */
+  mongoose.connect(dbURL);
+
+  /* Configure passport */
+  require('./config/passport')(passport);
+
+  /* Configure app */
+  app.use(express.static(pub));
+  app.use(morgan('dev'));
+  app.use(cookieParser());
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.set('views',__dirname + '/views');
+  app.set('view engine','jade');
+
+  app.use(session({ resave: false, saveUninitialized: false, secret: 'stockmarketprediction'}));
+  app.use(passport.initialize());
+  app.use(passport.session());
+  app.use(flash());
+  app.use(function(req, res, next) {
+    res.locals.message = req.flash();
+    next();
+  });
+
+  /* Perform app routings */
+  routes(app, passport);
+
+  return app;
+}
+
+module.exports = {
+  getApp: getApp,
+  port: port 
+};
